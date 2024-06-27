22e12e25d044e8dcc6449d0d81a7086a7d5275de
SeshanDev <seshan.nethmika@gmail.com>
2024-06-27 14:42:34 +0530

    first commit

---
+++ src/Components/DescriptopnBox/DescriptopnBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptopnBox.css'
+
+const DescriptopnBox = () => {
+return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (130)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+            Discover a treasure trove of goods ranging from fashion and beauty to electronics, home essentials, and beyond. Whether you're searching for the latest trends in fashion, upgrading your tech gadgets, or sprucing up your living space, we have everything you need to elevate your lifestyle.
+            Browse through our user-friendly interface, meticulously designed to make your shopping experience enjoyable and hassle-free. With intuitive search and filtering options, finding the perfect item has never been easier. Our comprehensive product descriptions and high-quality images ensure that you know exactly what you're getting before you make a purchase.
+            </p>
+            <p>
+            Join our community of savvy shoppers and experience the convenience of online shopping like never before. Whether you're treating yourself or finding the perfect gift for a loved one, [Website Name] is your go-to destination for all things fabulous.
+            Start exploring now and let the adventure begin!
+            </p>
+        </div>
+    </div>
+)
+}
+
+export default DescriptopnBox
